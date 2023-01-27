e75e16f9d0580f3659eb551223775e32f07eab1e
Prabal <prabal.jain@daffodilsw.com>
2023-01-28 02:31:35 +0530

    task-manager changes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const tasks = require('./routes/tasks')
+const notFound = require('./middleware/not-found')
 const connectDb = require('./db/connect')
+const errorHandlerMiddleware = require('./middleware/errorHandler')
 require('dotenv').config()
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
+//routes
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
 app.get('/hello', (req, res) => {
     res.send('<h1>TASK MANAGER APP</h1>')
 })
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,20 +1,55 @@
-const getAllTasks = (req, res) => {
-    res.send('All Items')
-}
+const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-errors')
 
-const createTask = (req, res) => {
-    console.log(req.body);
-    res.send('Create Task')
-}
-const getSingleTask = (req, res) => {
-    res.send('Get Single Task')
-}
-const updateTask = (req, res) => {
-    res.send('Update Task')
-}
-const deleteTask = (req, res) => {
-    res.send('Delete Task')
-}
+const getAllTasks = asyncWrapper(async (req, res, next) => {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks, nbHits: tasks.length })
+})
+
+const createTask = asyncWrapper(async (req, res, next) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+})
+
+const getSingleTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params
+    if (!taskID) {
+        return next(createCustomError(`ID is required`, 401))
+    }
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+        return next(createCustomError(`No task with ID : ${taskID}`, 404))
+    }
+    res.status(200).json({ task })
+})
+
+const updateTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params
+    if (!taskID) {
+        return next(createCustomError(`ID is required`, 401))
+    }
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true,
+        runValidators: true
+    })
+    if (!task) {
+        return next(createCustomError(`No task with ID : ${taskID}`, 404))
+    }
+    res.status(200).json({ id: taskID, data: task })
+})
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params
+    if (!taskID) {
+        return next(createCustomError(`ID is required`, 401))
+    }
+    const task = await Task.findByIdAndDelete({ _id: taskID })
+    if (!task) {
+        return next(createCustomError(`No task with ID : ${taskID}`, 404))
+    }
+    res.status(200).json({ task })
+})
 
 module.exports = {
     getAllTasks,
